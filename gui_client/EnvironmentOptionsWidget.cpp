7853a4a3355259f43789fa2603515b38fc1c5da0
Denis Shipilov <denshipilov@gmail.com>
2025-10-10 22:40:15 +0700

    Fix Northern Lights toggle: prevent aurora texture caching when disabled
    - Fixed issue where Northern Lights checkbox could not turn off aurora after being enabled once
    - Changed texture binding logic to use dummy_black_tex when draw_aurora=false
    - Added debug output for troubleshooting
    - Northern Lights now properly turns on/off via Environment panel checkbox

--- gui_client/EnvironmentOptionsWidget.cpp
+++ gui_client/EnvironmentOptionsWidget.cpp
@@ -22,6 +22,7 @@ EnvironmentOptionsWidget::EnvironmentOptionsWidget(QWidget* parent)
 
 	connect(this->sunThetaRealControl,	SIGNAL(valueChanged(double)),	this, SLOT(settingChangedSlot()));
 	connect(this->sunPhiRealControl,	SIGNAL(valueChanged(double)),	this, SLOT(settingChangedSlot()));
+	connect(this->northernLightsCheckBox,	SIGNAL(toggled(bool)),		this, SLOT(settingChangedSlot()));
 }
 
 
@@ -31,6 +32,7 @@ void EnvironmentOptionsWidget::init(QSettings* settings_) // settings should be 
 
 	SignalBlocker::setValue(this->sunThetaRealControl, settings->value("environment_options/sun_theta", /*default val=*/40.0).toDouble());
 	SignalBlocker::setValue(this->sunPhiRealControl,   settings->value("environment_options/sun_phi",   /*default val=*/0.0 ).toDouble());
+	this->northernLightsCheckBox->setChecked(settings->value("environment_options/northern_lights", /*default val=*/true).toBool());
 }
 
 
@@ -45,7 +47,18 @@ void EnvironmentOptionsWidget::settingChangedSlot()
 	{
 		settings->setValue("environment_options/sun_theta", this->sunThetaRealControl->value());
 		settings->setValue("environment_options/sun_phi",   this->sunPhiRealControl->value());
+		settings->setValue("environment_options/northern_lights", this->northernLightsCheckBox->isChecked());
 	}
 
+	// Debug: print when setting changed
+	printf("[EnvironmentOptionsWidget] settingChangedSlot called, northern_lights = %s\n", 
+		this->northernLightsCheckBox->isChecked() ? "true" : "false");
+
 	emit settingChanged();
+}
+
+
+bool EnvironmentOptionsWidget::getNorthernLightsEnabled() const
+{
+	return this->northernLightsCheckBox->isChecked();
 }
