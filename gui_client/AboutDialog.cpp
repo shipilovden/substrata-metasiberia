743471d7103a2ae2fee74385af06230880d631c9
Denis Shipilov <denshipilov@gmail.com>
2025-10-10 11:04:25 +0700

    Add About dialog with translation support

--- gui_client/AboutDialog.cpp
+++ gui_client/AboutDialog.cpp
@@ -17,18 +17,19 @@ AboutDialog::AboutDialog(QWidget* parent, const std::string& appdata_path)
 {
 	setupUi(this);
 
-	std::string display_str = "<h2>Substrata v" + ::cyberspace_version + "</h2>";
+	// Use tr() for translatable strings
+	QString display_str = "<h2>" + tr("Metasiberia v%1").arg(QString::fromStdString(::cyberspace_version)) + "</h2>";
 
 	display_str += "<p>";
-	display_str += QtUtils::toIndString(tr("Copyright Glare Technologies Limited."));
+	display_str += tr("Metasiberia is inspired and based on <a href=\"https://substrata.info\"><span style=\" text-decoration: underline; color:#222222;\">Substrata</span></a>.<br>");
+	display_str += tr("Forked by <a href=\"https://x.com/denshipilovart\"><span style=\" text-decoration: underline; color:#222222;\">Denis Shipilov</span></a>");
 	display_str += "</p>";
-	display_str += "<a href=\"https://substrata.info\"><span style=\" text-decoration: underline; color:#222222;\">http://substrata.info</span></a>";
 
-	this->text->setText(QtUtils::toQString(display_str));
+	this->text->setText(display_str);
 	this->text->setOpenExternalLinks(true);
 
 #if BUILD_TESTS
-	this->generateCrashLabel->setText("<p><a href=\"#\">Generate Crash</a></p>");
+	this->generateCrashLabel->setText("<p><a href=\"#\">" + tr("Generate Crash") + "</a></p>");
 #else
 	this->generateCrashLabel->hide();
 #endif
