06239fc0a7b9af2e727390c96a670ba5409452d1
Denis Shipilov <denshipilov@gmail.com>
2025-10-11 15:07:48 +0700

    Improve About dialog design with separators and bold links
    - Made all social media links bold (Github, Vk, Telegram, X/Twitter)
    - Made Substrata link bold
    - Added horizontal separator lines between sections
    - Moved author social links under 'Forked by Denis Shipilov'
    - Updated author links to personal profiles (X/Twitter, Vk, Telegram)
    - Improved visual hierarchy and readability

--- gui_client/AboutDialog.cpp
+++ gui_client/AboutDialog.cpp
@@ -20,16 +20,30 @@ AboutDialog::AboutDialog(QWidget* parent, const std::string& appdata_path)
 	// Use tr() for translatable strings
 	QString display_str = "<h2>" + tr("Metasiberia v%1").arg(QString::fromStdString(::cyberspace_version)) + "</h2>";
 
-	// Add social media links
+	// Add social media links for Metasiberia
 	display_str += "<p>";
-	display_str += "<a href=\"https://github.com/shipilovden/substrata-metasiberia\"><span style=\" text-decoration: underline; color:#222222;\">Github</span></a> ";
-	display_str += "<a href=\"https://vk.com/metasiberia_official\"><span style=\" text-decoration: underline; color:#222222;\">Vk</span></a> ";
-	display_str += "<a href=\"https://t.me/metasiberia_channel\"><span style=\" text-decoration: underline; color:#222222;\">Telegram</span></a>";
+	display_str += "<a href=\"https://github.com/shipilovden/substrata-metasiberia\"><span style=\" text-decoration: underline; color:#222222; font-weight: bold;\">Github</span></a> ";
+	display_str += "<a href=\"https://vk.com/metasiberia_official\"><span style=\" text-decoration: underline; color:#222222; font-weight: bold;\">Vk</span></a> ";
+	display_str += "<a href=\"https://t.me/metasiberia_channel\"><span style=\" text-decoration: underline; color:#222222; font-weight: bold;\">Telegram</span></a>";
 	display_str += "</p>";
 
+	// Add separator line
+	display_str += "<hr style=\"border: 1px solid #ccc; margin: 10px 0;\">";
+
+	// Add Substrata info with bold link
 	display_str += "<p>";
-	display_str += tr("Metasiberia is inspired and based on <a href=\"https://substrata.info\"><span style=\" text-decoration: underline; color:#222222;\">Substrata</span></a>.<br>");
-	display_str += tr("Forked by <a href=\"https://x.com/denshipilovart\"><span style=\" text-decoration: underline; color:#222222;\">Denis Shipilov</span></a>");
+	display_str += tr("Metasiberia is inspired and based on <a href=\"https://substrata.info\"><span style=\" text-decoration: underline; color:#222222; font-weight: bold;\">Substrata</span></a>.");
+	display_str += "</p>";
+
+	// Add separator line
+	display_str += "<hr style=\"border: 1px solid #ccc; margin: 10px 0;\">";
+
+	// Add author info with social links below
+	display_str += "<p>";
+	display_str += tr("Forked by Denis Shipilov<br>");
+	display_str += "<a href=\"https://x.com/denshipilovart\"><span style=\" text-decoration: underline; color:#222222; font-weight: bold;\">X/Twitter</span></a> ";
+	display_str += "<a href=\"https://vk.com/denshipilovart\"><span style=\" text-decoration: underline; color:#222222; font-weight: bold;\">Vk</span></a> ";
+	display_str += "<a href=\"https://t.me/denshipilovart\"><span style=\" text-decoration: underline; color:#222222; font-weight: bold;\">Telegram</span></a>";
 	display_str += "</p>";
 
 	this->text->setText(display_str);
